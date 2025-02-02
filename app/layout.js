42d9b4a3e089184d2af00b8681ea05df748ed571
Hasnain Arif <hasnainarif571@gmail.com>
2025-02-02 20:42:45 +0500

    Initial commit from Create Next App

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
