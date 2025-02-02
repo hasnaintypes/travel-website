0a6d2b7168b6cd90b609017dac396d1eaf3a9ef8
Hasnain Arif <hasnainarif571@gmail.com>
2025-02-02 21:01:45 +0500

    Remove unused SVG files and update Navbar import path

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,15 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
+export const metadata = {
+  title: "Hilink - Explore the World with Ease",
+  description:
+    "Hilink is your ultimate travel companion, helping you discover and explore amazing destinations effortlessly.",
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <Navbar />
         <main className="relative overflow-hidden">{children}</main>
